3c4c9fa6f9d827e4aa24bb7f8a301cf913fffc15
AlexanderMandrov <sashamandrovp@gmail.com>
2022-04-22 15:07:42 +0300

    I have finished DynamicArray or almost finished

--- DynamicArray.hpp
+++ DynamicArray.hpp
@@ -1,6 +1,8 @@
 #ifndef __DYNAMICARRAY__
 #define __DYNAMICARRAY__
 
+#include <iostream>
+#include <cstring>
 #include <stdexcept>
 
 template <typename T>
@@ -14,7 +16,7 @@ class DynamicArray {
        DynamicArray(T* items, int count);
        DynamicArray(int size);
        DynamicArray();
-       DynamicArray(const DynamicArray<T> & dynamicArray); 
+       DynamicArray(const DynamicArray<T> &dynamicArray); 
 
        ~DynamicArray();
 
@@ -41,13 +43,18 @@ template <typename T>
 DynamicArray<T>::DynamicArray() : count(0), items(nullptr) {}
 
 template <typename T>
-DynamicArray<T>::DynamicArray(const DynamicArray<T> & dynamicArray) {
-    this->count = dynamicArray.GetSize();
+DynamicArray<T>::DynamicArray(const DynamicArray<T> &dynamicArray) {
+
+    std::cout << "zero" << std::endl;
+
+    this->count = dynamicArray.count;
 
     if (this->count) {
         this->items = new T[this->count];
         memcpy(this->items, dynamicArray.items, this->count * sizeof(T));
+        std::cout << "second" << std::endl;
     } else {
+        std::cout << "third" << std::endl;
         this->items = nullptr;
     }
 }
@@ -81,7 +88,7 @@ void DynamicArray<T>::Set(int index, T value) {
         throw std::out_of_range("Out of the range of the array");
     }
 
-    memcpy(this->items + index * sizeof(T), &value, sizeof(T));
+    memcpy(this->items + index, &value, sizeof(T));
 }
 
 template <typename T>
@@ -90,7 +97,7 @@ void DynamicArray<T>::Resize(int newSize) {
         throw std::invalid_argument("invalid value");
     }
 
-    if (!newSize) {
+    if (newSize) {
         T* bufer = new T[newSize];
         memcpy(bufer, this->items, this->count * sizeof(T));
         delete[] this->items;
--- test.cpp
+++ test.cpp
@@ -5,7 +5,51 @@ using namespace std;
 
 int main() {
 
-    cout << "Hello World!" << endl;
+    int array[] = {1, 2, 3, 4, 5};
+
+    DynamicArray<int>* example1 = new DynamicArray<int>(array, 5);
+
+    cout << "Size: " << example1->GetSize() << endl;
+
+     for (int i = 0; i < 5; i++) {
+        cout << example1->Get(i) << endl;
+    }
+
+    DynamicArray<int>* example2 = new DynamicArray<int>(*example1);
+
+    cout << endl;
+
+    for (int i = 0; i < 5; i++) {
+        cout << example1->Get(i) << endl;
+    }
+    
+    cout << "Size: " << example2->GetSize() << endl;
+
+    example2->Resize(7);
+    
+    for (int i = 0; i < 7; i++) {
+        example2->Set(i, 7 - i);
+    }
+
+    for (int i = 0; i < 7; i++) {
+        cout << example2->Get(i) << endl;
+    }
+
+    cout << endl;
+
+    for (int i = 0; i < 5; i++) {
+        cout << example1->Get(i) << endl;
+    }
+
+    cout << endl;
+
+    for (int i = 0; i < 7; i++) {
+        cout << example2->Get(i) << endl;
+    }
+
+
+    delete example1;
+    delete example2;
 
     return 0;
 
